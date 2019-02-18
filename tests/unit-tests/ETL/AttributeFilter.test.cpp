f90baded729cb76664767a5799df4bad8a44e4ae
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-18 22:34:59 +0100

    [test] added AttributeFilter test

--- tests/unit-tests/ETL/AttributeFilter.test.cpp
+++ tests/unit-tests/ETL/AttributeFilter.test.cpp
@@ -1,4 +1,61 @@
-//
-// Created by vwallyn on 18/02/19.
-//
+#define CATCH_CONFIG_MAIN   // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch2/catch.hpp"
+#include <string>
+#include <vector>
+#include "../../../src/ETL/include/AttributeFilter.h"
+#include "../../utils.hpp"
+
+using namespace std;
+
+// TODO: add test id
+
+TEST_CASE("Testing AttributeFilter::addAttribute", "[]") {
+    AttributeFilter attributeFilter1;
+
+    vector<string> attributes;
+
+    for (int i = 0; i < 10; ++i) {
+        string attribute = randomString(10);
+
+        attributeFilter1.addAttribute(attribute);
+        attributes.push_back(attribute);
+    }
+
+    CHECK(attributeFilter1.apply() == "sensorID in (" + join(attributes) + ")" );
+}
+
+TEST_CASE("Testing AttributeFilter::addAttributes", "[]") {
+    AttributeFilter attributeFilter1;
+
+    vector<string> attributes;
+
+    for (int i = 0; i < 10; ++i) {
+        string attribute = randomString(10);
+
+        attributes.push_back(attribute);
+    }
+    attributeFilter1.addAttributes(attributes);
+
+    CHECK(attributeFilter1.apply() == "sensorID in (" + join(attributes) + ")" );
+}
+
+
+
+TEST_CASE("Testing TimeFilter '=' operator", "[]") {
+    AttributeFilter attributeFilter1, attributeFilter2;
+
+    attributeFilter1.addAttribute(randomString(10));
+    attributeFilter1 = attributeFilter2;
+
+    SECTION("Equality") {
+        CHECK(attributeFilter1.apply() == attributeFilter2.apply());
+    }
+
+    attributeFilter1.addAttribute(randomString(10));
+
+    SECTION("Non-Equality") {
+        CHECK(attributeFilter1.apply() != attributeFilter2.apply());
+    }
+}
 
