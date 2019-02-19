099f81b935ad723299efd2f84ea35ad1846f3f7d
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-19 23:12:44 +0100

    [feat] added test tags

--- tests/unit-tests/ETL/GeoFilter.test.cpp
+++ tests/unit-tests/ETL/GeoFilter.test.cpp
@@ -4,9 +4,7 @@
 
 using namespace std;
 
-// TODO: add test id
-
-TEST_CASE("Testing GeoFilter::setBBox with normal BBox", "[]") {
+TEST_CASE("Testing GeoFilter::setBBox with normal BBox", "[UT-E-6]") {
     GeoFilter geoFilter1;
 
     BBox b = { 45, 4, 50, 30 };
@@ -16,7 +14,7 @@ TEST_CASE("Testing GeoFilter::setBBox with normal BBox", "[]") {
     CHECK(geoFilter1.apply() == "longitude >= 45 AND latitude >= 4 AND longitude <= 50 AND latitude <= 30");
 }
 
-TEST_CASE("Testing GeoFilter::setBBox with equal BBox", "[]") {
+TEST_CASE("Testing GeoFilter::setBBox with equal BBox", "[UT-E-6]") {
     GeoFilter geoFilter1;
 
     BBox b = {10, 10, 10, 10};
@@ -26,7 +24,7 @@ TEST_CASE("Testing GeoFilter::setBBox with equal BBox", "[]") {
     CHECK(geoFilter1.apply() == "longitude >= 10 AND longitude <= 10 AND latitude >= 10 AND latitude <= 10");
 }
 
-TEST_CASE("Testing GeoFilter::setBBox with bad BBox", "[]") {
+TEST_CASE("Testing GeoFilter::setBBox with bad BBox", "[UT-E-6]") {
     GeoFilter geoFilter1;
 
     BBox b = {10, 11, -20, 21};
@@ -36,7 +34,7 @@ TEST_CASE("Testing GeoFilter::setBBox with bad BBox", "[]") {
     REQUIRE_THROWS(geoFilter1.apply());
 }
 
-TEST_CASE("Testing GeoFilter::extend with ratio=2", "[]") {
+TEST_CASE("Testing GeoFilter::extend with ratio=2", "[UT-E-7]") {
     GeoFilter geoFilter1;
 
     BBox b = { 45, 4, 50, 30 };
@@ -47,7 +45,7 @@ TEST_CASE("Testing GeoFilter::extend with ratio=2", "[]") {
     CHECK(geoFilter1.apply() == "longitude >= 22.5 AND latitude >= 2 AND longitude <= 100 AND latitude <= 60");
 }
 
-TEST_CASE("Testing GeoFilter::extend with ratio=0.5", "[]") {
+TEST_CASE("Testing GeoFilter::extend with ratio=0.5", "[UT-E-7]") {
     GeoFilter geoFilter1;
 
     BBox b = { 45, 4, 50, 30 };
@@ -58,7 +56,7 @@ TEST_CASE("Testing GeoFilter::extend with ratio=0.5", "[]") {
     CHECK(geoFilter1.apply() == "longitude >= 67.5 AND latitude >= 6 AND longitude <= 25 AND latitude <= 15");
 }
 
-TEST_CASE("Testing GeoFilter::extend with ratio=-2", "[]") {
+TEST_CASE("Testing GeoFilter::extend with ratio=-2", "[UT-E-7]") {
     GeoFilter geoFilter1;
 
     BBox b = { 45, 4, 50, 30 };
