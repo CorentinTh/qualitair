d143c1d770363cba64d7bc6d3ace02c349fe9402
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-02-19 22:44:23 +0100

    [fix] syntax error preventing the build

--- src/View/include/IOutput.h
+++ src/View/include/IOutput.h
@@ -12,7 +12,7 @@ class IOutput {
     public:
         virtual ~IOutput() {}
         virtual void printSpikes(std::string dataJson, std::string filename) = 0;
-        virtual void printStats(std::string dataJson, std::string filename= = 0;
+        virtual void printStats(std::string dataJson, std::string filename) = 0;
         virtual void printSim(std::string dataJson, std::string filename) = 0;
         virtual void printBroken(std::string dataJson, std::string filename) = 0;
         virtual void printIngest(std::string dataJson, std::string filename) = 0;
