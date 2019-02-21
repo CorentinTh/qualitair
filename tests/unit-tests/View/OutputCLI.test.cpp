daf4fcbf493bf60d9012a8921b52ecf4c76057fb
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-02-21 22:10:14 +0100

    [feat] unit-test : corrected data type (from std::string to json), deleted useless getInstance() test

--- tests/unit-tests/View/OutputCLI.test.cpp
+++ tests/unit-tests/View/OutputCLI.test.cpp
@@ -8,91 +8,84 @@
 #include <fstream>
 #include <istream>
 #include <typeinfo>
-
+#include <nlohmann/json.hpp>
 #include "../../../src/View/include/OutputCLI.h"
 
-std::string dataJsonSpikes = "{ \
-    \"timestamp\" :  1550150155, \
-    \"latitude\" : 48.8534, \
-    \"longitude\" : 2.3488, \
-    \"pics\" : { \
-            { \
-                { \
-                    0, \
-                    1 \
-                },\
-                {\
-                    0, \
-                    0 \
-                } \
-            }, \
-            { \
-                { \
-                    0, \
-                    1 \
-                }, \
-                { \
-                    0, \
-                    0 \
-                } \
-            } \
-    } \
-};";
-std::string dataJsonStats = "{ \
-  \"co2\" : { \
-      \"avg\":6, \
-      \"min\":2, \
-      \"max\":10, \
-      \"deviation\":2.62 \
-  }, \
-  \"o2\":{ \
-      \"avg\":4.88, \
-      \"min\":1, \
-      \"max\":10, \
-      \"deviation\":2.70, \
-  }, \
-  \"igqa\": 0.55 \
-};";
-std::string dataJsonSim = "{ \
-  { \
-    { \
-      \"id\":1, \
-      \"lat\":45.7574995, \
-      \"long\":4.8313017, \
-      \"description\":\"Bellecour - Grande roue\" \
-    }, \
-    { \
-      \"id\":3, \
-      \"lat\":45.762994, \
-      \"long\":4.833632, \
-      \"description\":\"Rue de la république\" \
-    } \
-  } \
-}";
-std::string dataJsonBroken = "{ \
-  {  \
-    \"id\":71, \
-    \"lat\":48.597855, \
-    \"long\":3.401035, \
-    \"description\":\"Pétaouchnok\" \
-  }, \
-  {  \
-    \"id\":147,  \
-    \"lat\":28.468412, \
-    \"long\":14.351684, \
-    \"description\":\"Paris - Tour Eiffel\" \
-  } \
-}";
-std::string dataJsonIngest = "{ \
-    \"lines_inserted\":4201 \
-    \"error\":"" \
-}";
+using json = nlohmann::json;
+
+
+json dataJsonSpikes = {
+        {"timestamp",  1550150155},
+        {"latitude", 48.8534},
+        {"longitude", 2.3488},
+        {"pics", {
+                         {
+                                 {0, 1},
+                                 {0, 0}
+                         },
+                         {
+                                 {0, 1},
+                                 {0, 0}
+                         }
+                 }
+        }
+};
+json dataJsonStats = {
+        {"co2", {
+                {"avg",6},
+                {"min",2},
+                {"max",10},
+                {"deviation",2.62}
+        }
+        },
+        {"o2", {
+                {"avg", 4.88},
+                {"min", 1},
+                {"max", 10},
+                {"deviation", 2.70},
+        }
+        },
+        {"igqa", 0.55}
+};
+json dataJsonSim = {
+      {
+            {
+                  {"id",1},
+                  {"lat",45.7574995},
+                  {"long",4.8313017},
+                  {"description","Bellecour - Grande roue"}
+            },
+            {
+                  {"id",3},
+                  {"lat",45.762994},
+                  {"long",4.833632},
+                  {"description","Rue de la république"}
+            }
+      }
+};
+json dataJsonBroken = {
+      {
+            {"id",71},
+            {"lat",48.597855},
+            {"long",3.401035},
+            {"description","Pétaouchnok"}
+      },
+      {
+            {"id",147},
+            {"lat"},28.468412,
+            {"long",14.351684},
+            {"description","Paris - Tour Eiffel"}
+      }
+};
+json dataJsonIngest = {
+    {"lines_inserted",4201},
+    {"error",""}
+};
 
 
 TEST_CASE("Test printSpikes(dataJSON) CLI", "[UT-V-1]") {
 
     OutputCLI myOutputGenerator = OutputCLI::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputCLI");
     SECTION("is the method returning something"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printSpikes(dataJsonSpikes)), void>::value);
     }
@@ -118,7 +111,6 @@ TEST_CASE("Test printSpikes(dataJSON) CLI", "[UT-V-1]") {
 TEST_CASE("Test printStats(dataJSON) CLI", "[UT-V-2]") {
 
     OutputCLI myOutputGenerator = OutputCLI::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputCLI");
     SECTION("is the method returning something"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printStats(dataJsonStats)), void>::value);
     }
@@ -144,8 +136,7 @@ TEST_CASE("Test printStats(dataJSON) CLI", "[UT-V-2]") {
 TEST_CASE("Test printSim(dataJSON) CLI", "[UT-V-3]") {
 
     OutputCLI myOutputGenerator = OutputCLI::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputCLI");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printSim(dataJsonSim)), void>::value);
     }
     SECTION("is the method putting something in cout"){
@@ -170,8 +161,7 @@ TEST_CASE("Test printSim(dataJSON) CLI", "[UT-V-3]") {
 TEST_CASE("Test printBroken(dataJSON) CLI", "[UT-V-4]") {
 
     OutputCLI myOutputGenerator = OutputCLI::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputCLI");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printBroken(dataJsonBroken)), void>::value);
     }
     SECTION("is the method putting something in cout"){
@@ -196,8 +186,7 @@ TEST_CASE("Test printBroken(dataJSON) CLI", "[UT-V-4]") {
 TEST_CASE("Test printIngest(dataJSON) CLI", "[UT-V-5]") {
 
     OutputCLI myOutputGenerator = OutputCLI::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputCLI");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printIngest(dataJsonIngest)), void>::value);
     }
     SECTION("is the  method putting something in cout"){
--- tests/unit-tests/View/OutputHTML.test.cpp
+++ tests/unit-tests/View/OutputHTML.test.cpp
@@ -12,89 +12,79 @@
 #include "../../../src/View/include/OutputHTML.h"
 
 std::string jsonFilename = "mon_rapport_qualitair.html";
-std::string dataJsonSpikes = "{ \
-    \"timestamp\" :  1550150155, \
-    \"latitude\" : 48.8534, \
-    \"longitude\" : 2.3488, \
-    \"pics\" : { \
-            { \
-                { \
-                    0, \
-                    1 \
-                },\
-                {\
-                    0, \
-                    0 \
-                } \
-            }, \
-            { \
-                { \
-                    0, \
-                    1 \
-                }, \
-                { \
-                    0, \
-                    0 \
-                } \
-            } \
-    } \
-};";
-std::string dataJsonStats = "{ \
-  \"co2\" : { \
-      \"avg\":6, \
-      \"min\":2, \
-      \"max\":10, \
-      \"deviation\":2.62 \
-  }, \
-  \"o2\":{ \
-      \"avg\":4.88, \
-      \"min\":1, \
-      \"max\":10, \
-      \"deviation\":2.70, \
-  }, \
-  \"igqa\": 0.55 \
-};";
-std::string dataJsonSim = "{ \
-  { \
-    { \
-      \"id\":1, \
-      \"lat\":45.7574995, \
-      \"long\":4.8313017, \
-      \"description\":\"Bellecour - Grande roue\" \
-    }, \
-    { \
-      \"id\":3, \
-      \"lat\":45.762994, \
-      \"long\":4.833632, \
-      \"description\":\"Rue de la république\" \
-    } \
-  } \
-}";
-std::string dataJsonBroken = "{ \
-  {  \
-    \"id\":71, \
-    \"lat\":48.597855, \
-    \"long\":3.401035, \
-    \"description\":\"Pétaouchnok\" \
-  }, \
-  {  \
-    \"id\":147,  \
-    \"lat\":28.468412, \
-    \"long\":14.351684, \
-    \"description\":\"Paris - Tour Eiffel\" \
-  } \
-}";
-std::string dataJsonIngest = "{ \
-    \"lines_inserted\":4201 \
-    \"error\":"" \
-}";
+json dataJsonSpikes = {
+        {"timestamp",  1550150155},
+        {"latitude", 48.8534},
+        {"longitude", 2.3488},
+        {"pics", {
+                               {
+                                       {0, 1},
+                                       {0, 0}
+                               },
+                               {
+                                       {0, 1},
+                                       {0, 0}
+                               }
+                       }
+        }
+};
+json dataJsonStats = {
+        {"co2", {
+                        {"avg",6},
+                        {"min",2},
+                        {"max",10},
+                        {"deviation",2.62}
+                }
+        },
+        {"o2", {
+                        {"avg", 4.88},
+                        {"min", 1},
+                        {"max", 10},
+                        {"deviation", 2.70},
+                }
+        },
+        {"igqa", 0.55}
+};
+json dataJsonSim = {
+        {
+                {
+                        {"id",1},
+                        {"lat",45.7574995},
+                        {"long",4.8313017},
+                        {"description","Bellecour - Grande roue"}
+                },
+                {
+                        {"id",3},
+                        {"lat",45.762994},
+                        {"long",4.833632},
+                        {"description","Rue de la république"}
+                }
+        }
+};
+json dataJsonBroken = {
+        {
+                {"id",71},
+                {"lat",48.597855},
+                        {"long",3.401035},
+                {"description","Pétaouchnok"}
+        },
+        {
+                {"id",147},
+                {"lat"},28.468412,
+                {"long",14.351684},
+                {"description","Paris - Tour Eiffel"}
+        }
+};
+json dataJsonIngest = {
+        {"lines_inserted",4201},
+        {"error",""}
+};
 
 
 TEST_CASE("Test printSpikes(dataJSON) HTML", "[UT-V-6]") {
 
     OutputHTML myOutputGenerator = OutputHTML::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputHTML");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printSpikes(dataJsonSpikes, jsonFilename)), void>::value);
     }
     SECTION("is the method creating the file"){
@@ -118,8 +108,7 @@ TEST_CASE("Test printSpikes(dataJSON) HTML", "[UT-V-6]") {
 TEST_CASE("Test printStats(dataJSON) HTML", "[UT-V-7]") {
 
     OutputHTML myOutputGenerator = OutputHTML::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputHTML");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printStats(dataJsonStats, jsonFilename)), void>::value);
     }
     SECTION("is the method creating the file"){
@@ -142,8 +131,7 @@ TEST_CASE("Test printStats(dataJSON) HTML", "[UT-V-7]") {
 TEST_CASE("Test printSim(dataJSON) HTML", "[UT-V-8]") {
 
     OutputHTML myOutputGenerator = OutputHTML::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputHTML");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printSim(dataJsonSim, jsonFilename)), void>::value);
     }
     SECTION("is the method creating the file"){
@@ -166,8 +154,7 @@ TEST_CASE("Test printSim(dataJSON) HTML", "[UT-V-8]") {
 TEST_CASE("Test printBroken(dataJSON) HTML", "[UT-V-9]") {
 
     OutputHTML myOutputGenerator = OutputHTML::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputHTML");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printBroken(dataJsonBroken, jsonFilename)), void>::value);
     }
     SECTION("is the method creating the file"){
@@ -190,8 +177,7 @@ TEST_CASE("Test printBroken(dataJSON) HTML", "[UT-V-9]") {
 TEST_CASE("Test printIngest(dataJSON) HTML", "[UT-V-10]") {
 
     OutputHTML myOutputGenerator = OutputHTML::getInstance();
-    REQUIRE(typeid(myOutputGenerator.getInstance()).name()=="OutputHTML");
-    SECTION("is the method returning something"){
+    SECTION("is the method returning nothing"){
         REQUIRE(std::is_same<decltype(myOutputGenerator.printIngest(dataJsonIngest, jsonFilename)), void>::value);
     }
     SECTION("is the method creating the file"){
