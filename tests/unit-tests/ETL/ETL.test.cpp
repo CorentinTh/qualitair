e5b13461dc576aecdee3cebd5904a1edd03652f0
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-21 20:55:56 +0100

    [chore] commit for merge

--- tests/unit-tests/ETL/ETL.test.cpp
+++ tests/unit-tests/ETL/ETL.test.cpp
@@ -2,11 +2,18 @@
 #include "../../../src/ETL/include/ETL.h"
 #include "../../utils.hpp"
 
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
 using namespace std;
 
 
 TEST_CASE("Testing ETL::ingest with clean csv string", "[UT-E-1]"){
     ETL etl = ETL::getInstance();
 
+
+    json ret = etl.ingest({
+
+    })
 
 }
