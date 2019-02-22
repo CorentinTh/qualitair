5f08f76703b19eaaa15b6dcfb4b1b63e39d92f1d
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-23 00:54:06 +0100

    [fix] better compiler

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-# Creation of make build directories
-mkdir -p cmake-build-debug
+dir='cmake-build-debug'
 
-cd cmake-build-debug
+# Creation of make build directories
+mkdir -p ${dir}
 
 # Building all targets
 
 if [ -z "$1" ]
   then
     echo "-> No argument supplied: building all targets."
-    cmake ../ -DBUILD_TESTING=OFF -DBUILD_TESTS=OFF
+    cmake --build ${dir} -- -j 4
   else
     echo "-> Building target $1"
-    cmake --target "$1" ../ -DBUILD_TESTING=OFF -DBUILD_TESTS=OFF
+    cmake --build ${dir} --target $1 -- -j 4
 fi
-
-make
