0c22145b3dacba75e8dc27b3bbadb89e17aaf51f
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-23 01:22:59 +0100

    [fix] better compiler

--- build.sh
+++ build.sh
@@ -2,16 +2,27 @@
 
 dir='cmake-build-debug'
 
-# Creation of make build directories
-mkdir -p ${dir}
 
-# Building all targets
-
-if [ -z "$1" ]
+if [[ -z "$1" ]]
   then
-    echo "-> No argument supplied: building all targets."
+  echo "Error, missing argument for build.sh"
+  exit 1
+
+elif ["$1" == "init"]; then
+
+    mkdir -p ${dir}
+    cd ${dir}
+    cmake ../ -DBUILD_TESTING=OFF
+    cd ..
+
+elif ["$1" == "all"]; then
+
+    echo "--> Building all targets."
     cmake --build ${dir} -- -j 4
-  else
-    echo "-> Building target $1"
+
+else
+
+    echo "--> Building target $1"
     cmake --build ${dir} --target $1 -- -j 4
+
 fi
