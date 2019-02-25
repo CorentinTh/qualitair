4549e8f79eee0d6589c5f5b085993232d9c17343
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-25 21:11:16 +0100

    [fix] removed beautifully and randomly generated sensors

--- tests/unit-tests/ETL/SensorFilter.test.cpp
+++ tests/unit-tests/ETL/SensorFilter.test.cpp
@@ -7,46 +7,40 @@
 using namespace std;
 
 TEST_CASE("Testing SensorFilter::addSensor", "[UT-E-8]") {
-    SensorFilter sensorFilter1;
+    SensorFilter sensorFilter;
 
-    vector<string> sensorsID;
+    sensorFilter.addSensor("sensor1");
+    sensorFilter.addSensor("sensor2");
+    sensorFilter.addSensor("sensor3");
 
-    for (int i = 0; i < 10; ++i) {
-        string sensorID = randomString(10);
-
-        sensorFilter1.addSensor(sensorID);
-        sensorsID.push_back(sensorID);
-    }
-
-    CHECK(sensorFilter1.apply() == "sensorID in (" + join(sensorsID) + ")" );
+    CHECK(sensorFilter.apply() == "sensorID in (sensor1, sensor2, sensor3)" );
 }
 
 TEST_CASE("Testing SensorFilter::addSensors", "[UT-E-9]") {
-    SensorFilter sensorFilter1;
+    SensorFilter sensorFilter;
 
-    vector<string> sensorsID;
+    vector<string> sensors;
 
-    for (int i = 0; i < 10; ++i) {
-        string sensorID = randomString(10);
+    sensors.emplace_back("sensor1");
+    sensors.emplace_back("sensor2");
+    sensors.emplace_back("sensor3");
 
-        sensorsID.push_back(sensorID);
-    }
-    sensorFilter1.addSensors(sensorsID);
+    sensorFilter.addSensors(sensors);
 
-    CHECK(sensorFilter1.apply() == "sensorID in (" + join(sensorsID) + ")" );
+    CHECK(sensorFilter.apply() == "sensorID in (sensor1, sensor2, sensor3)" );
 }
 
 TEST_CASE("Testing TimeFilter '=' operator", "[extra]") {
     SensorFilter sensorFilter1, sensorFilter2;
 
-    sensorFilter1.addSensor(randomString(10));
+    sensorFilter1.addSensor("sensor1");
     sensorFilter2 = sensorFilter1;
 
     SECTION("Equality") {
         CHECK(sensorFilter1.apply() == sensorFilter2.apply());
     }
 
-    sensorFilter1.addSensor(randomString(10));
+    sensorFilter1.addSensor("sensor2");
 
     SECTION("Non-Equality") {
         CHECK(sensorFilter1.apply() != sensorFilter2.apply());
