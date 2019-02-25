60d46de5ea86ec2eabf8661d5429efb0ef7f9bb7
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-25 21:06:49 +0100

    [fix] removed beautifully and randomly generated attributes

--- tests/unit-tests/ETL/AttributeFilter.test.cpp
+++ tests/unit-tests/ETL/AttributeFilter.test.cpp
@@ -9,16 +9,11 @@ using namespace std;
 TEST_CASE("Testing AttributeFilter::addAttribute", "[UT-E-8]") {
     AttributeFilter attributeFilter1;
 
-    vector<string> attributes;
+    attributeFilter1.addAttribute("attr1");
+    attributeFilter1.addAttribute("attr2");
+    attributeFilter1.addAttribute("attr3");
 
-    for (int i = 0; i < 10; ++i) {
-        string attribute = randomString(10);
-
-        attributeFilter1.addAttribute(attribute);
-        attributes.push_back(attribute);
-    }
-
-    CHECK(attributeFilter1.apply() == "sensorID in (" + join(attributes) + ")" );
+    CHECK(attributeFilter1.apply() == "sensorID in (attr1, attr2, attr3)" );
 }
 
 TEST_CASE("Testing AttributeFilter::addAttributes", "[UT-E-9]") {
@@ -26,14 +21,13 @@ TEST_CASE("Testing AttributeFilter::addAttributes", "[UT-E-9]") {
 
     vector<string> attributes;
 
-    for (int i = 0; i < 10; ++i) {
-        string attribute = randomString(10);
+    attributes.emplace_back("attr1");
+    attributes.emplace_back("attr2");
+    attributes.emplace_back("attr3");
 
-        attributes.push_back(attribute);
-    }
     attributeFilter1.addAttributes(attributes);
 
-    CHECK(attributeFilter1.apply() == "sensorID in (" + join(attributes) + ")" );
+    CHECK(attributeFilter1.apply() == "sensorID in (attr1, attr2, attr3)" );
 }
 
 TEST_CASE("Testing AttributeFilter '=' operator", "[extra]") {
