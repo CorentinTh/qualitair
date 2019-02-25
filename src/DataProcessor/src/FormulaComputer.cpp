835b1b47169696910815ca30390ab45ddf5579d0
Sadikk <youpi.killeur@gmail.com>
2019-02-25 22:22:57 +0100

    [feat] formula computer 1

--- src/DataProcessor/src/FormulaComputer.cpp
+++ src/DataProcessor/src/FormulaComputer.cpp
@@ -6,11 +6,12 @@
 
 
 FormulaComputer &FormulaComputer::operator=(FormulaComputer other) {
+    swap(*this, other);
     return *this;
 }
 
 FormulaComputer::FormulaComputer(const FormulaComputer &other) {
-
+    formula = other.formula;
 }
 
 
@@ -23,9 +24,25 @@ FormulaComputer::~FormulaComputer() {
 }
 
 double FormulaComputer::compute(pointCollection &data) {
+
+    /*typedef exprtk::symbol_table<double> symbol_table_d;
+    typedef exprtk::expression<double>     expression_d;
+    typedef exprtk::parser<double>             parser_d;
+
+    symbol_table_d symbol_table;
+
+    symbol_table.add_variable("x",x);
+    symbol_table.add_constants();
+
+    expression_d expression;
+    expression.register_symbol_table(symbol_table);
+
+    parser_d parser;
+    parser.compile(formula, expression);
+    */
     return 0;
 }
 
 void swap(FormulaComputer &first, FormulaComputer &second) {
-
+    std::swap(first.formula, second.formula);
 }
