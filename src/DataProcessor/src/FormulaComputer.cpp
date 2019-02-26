4e4c72819de2bda3c7f4c13d3808208748ad50a2
Sadikk <youpi.killeur@gmail.com>
2019-02-26 21:39:02 +0100

    [feat] FormulaComputer

--- src/DataProcessor/src/FormulaComputer.cpp
+++ src/DataProcessor/src/FormulaComputer.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "../include/FormulaComputer.h"
-
+#include "exprtk.hpp"
 
 FormulaComputer &FormulaComputer::operator=(FormulaComputer other) {
     swap(*this, other);
@@ -25,13 +25,33 @@ FormulaComputer::~FormulaComputer() {
 
 double FormulaComputer::compute(pointCollection &data) {
 
-    /*typedef exprtk::symbol_table<double> symbol_table_d;
+    typedef exprtk::symbol_table<double> symbol_table_d;
     typedef exprtk::expression<double>     expression_d;
     typedef exprtk::parser<double>             parser_d;
 
     symbol_table_d symbol_table;
 
-    symbol_table.add_variable("x",x);
+    std::unordered_map<std::string, double> sums;
+
+    for (auto i = data.begin(); i != data.end() ; ++i)
+    {
+        for (auto j = i->begin(); j != i->end() ; ++j)
+        {
+            for (auto k = j->begin(); k != j->end() ; ++k)
+            {
+                for (std::unordered_map<std::string, int>::const_iterator it = k->begin();
+                     it != k->end(); ++it)
+                {
+                    sums[it->first] += it->second;
+                }
+            }
+        }
+    }
+
+    for (auto it = sums.begin(); it!= sums.end(); ++it) {
+        symbol_table.add_variable(it->first, it->second);
+    }
+
     symbol_table.add_constants();
 
     expression_d expression;
@@ -39,8 +59,8 @@ double FormulaComputer::compute(pointCollection &data) {
 
     parser_d parser;
     parser.compile(formula, expression);
-    */
-    return 0;
+
+    return std::floor((expression.value()) * 100.0 + 0.5) / 100.0;
 }
 
 void swap(FormulaComputer &first, FormulaComputer &second) {
