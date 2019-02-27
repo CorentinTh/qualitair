390d1f7a4363f1e0936d391d803eea598d1c1f22
CorentinTh <corentin.thomasset74@gmail.com>
2019-02-27 08:09:04 +0100

    [feat] codecoverage

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,6 @@
 
 
 lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info "/usr/*" "${HOME}/.cache/*" "./deps/*" --output-file coverage.info
+lcov --remove coverage.info "/usr/*" "${HOME}/.cache/*" --output-file coverage.info
 lcov --list coverage.info
 bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
