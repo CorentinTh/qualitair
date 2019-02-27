1b698fe19efa3612b4f8a6e0b7c45c135dbda43d
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-02-27 14:00:31 +0100

    [feat] implemented printSpikes of OutputCLI

--- src/View/src/OutputCLI.cpp
+++ src/View/src/OutputCLI.cpp
@@ -4,12 +4,93 @@
 
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include "../include/OutputCLI.h"
 
 OutputCLI::OutputCLI() {}
 
-void OutputCLI::printSpikes(json dataJson, std::string filename) {
+typedef struct {
+    double x;
+    double y;
+    int tStart;
+    int tEnd;
+} Spike;
 
+void OutputCLI::printSpikes(json dataJson, std::string filename) {
+    std::vector<Spike*> vectSpikes;
+    if (!dataJson.empty() && dataJson.find("pics") != dataJson.end()) {
+        std::cout << "Des pics ont été détectés :" << std::endl;
+        double t0, x0, y0;
+        int deltaS, deltaT;
+        t0 = dataJson.at("t0");
+        x0 = dataJson.at("x0");
+        y0 = dataJson.at("y0");
+        deltaS = dataJson.at("deltaSpatial");
+        deltaT = dataJson.at("deltaTemp");
+
+        int indiceT = 0; // time index
+        for (json::iterator itTimeList = dataJson.at("pics").begin();
+                itTimeList != dataJson.at("pics").end(); ++itTimeList) {
+
+            int indiceY = 0; // longitude index
+            for (json::iterator itY = (*itTimeList).begin(); itY != (*itTimeList).end(); ++itY) {
+                int indiceX = 0; // latitude index
+                for (json::iterator itX = (*itY).begin(); itX != (*itY).end(); ++itX) {
+                    if (*itX == 1) {
+                        bool spikeInVector = false;
+                        for (Spike* s : vectSpikes) {
+                            if (s->x == x0 + indiceX * deltaS
+                                    && s->y == y0 + indiceY * deltaS) {
+                                if (s->tStart < (int) t0 + indiceT * deltaT
+                                        && s->tEnd == (int) t0 + (indiceT - 1) * deltaT) {
+                                    // if the spike is already in the spikes vector
+                                    // we extend its duration
+                                    s->tEnd = (int) t0 + (indiceT) * deltaT;
+                                    spikeInVector = true;
+                                }
+                                else {
+                                    spikeInVector = false;
+                                }
+                            }
+                        }
+                        if (!spikeInVector) {
+                            // if the spike is not in the spikes vector
+                            // we create it and add it
+                            Spike * newSpike = new Spike;
+                            newSpike->tEnd = (int) t0 + (indiceT) * deltaT;
+                            newSpike->tStart = (int) t0 + (indiceT) * deltaT;
+                            newSpike->x = x0 + indiceX * deltaS;
+                            newSpike->y = y0 + indiceY * deltaS;
+                            vectSpikes.push_back(newSpike);
+                        }
+                    }
+                    indiceX++;
+                }
+                indiceY++;
+            }
+            indiceT++;
+        }
+
+        for (Spike * s : vectSpikes) {
+
+            time_t tEnd = (time_t) s->tEnd;
+            struct tm *tmEnd = localtime(&tEnd);
+            char dateEnd[25];
+            strftime(dateEnd, sizeof(dateEnd), "%H:%M:%S le %d/%m/%Y", tmEnd);
+
+            time_t tStart = (time_t) s->tStart;
+            struct tm *tmStart = localtime(&tStart);
+            char dateStart[25];
+            strftime(dateStart, sizeof(dateStart), "%H:%M:%S le %d/%m/%Y", tmStart);
+
+            std::cout << std::setprecision(8);
+            std::cout << " - en position (" << s->x << "," << s->y << ")";
+            std::cout << " entre " << dateStart << " et " << dateEnd << std::endl;
+
+            // delete the pointers
+            delete(s);
+        }
+    }
 }
 
 void OutputCLI::printStats(json dataJson, std::string filename) {
