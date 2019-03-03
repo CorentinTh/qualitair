cd991e01eb0110b8c20883a56c348644b3c81589
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-03-03 15:10:24 +0100

    [fix] use .where() before .orWhere() !!!

--- src/ETL/src/AttributeFilter.cpp
+++ src/ETL/src/AttributeFilter.cpp
@@ -6,15 +6,24 @@
 #include <algorithm>
 
 void AttributeFilter::applyTo(QueryBuilder &qb){
+    int compteur = 0;
     for (std::string attr : this->attributes){
-        qb.orWhere("AttributeId = ?").bind(attr);
+        if (compteur == 0){
+            qb.where("attributeId = ?").bind(attr);
+        }
+        else{
+            qb.orWhere("attributeId = ?").bind(attr);
+        }
+        compteur++;
     }
 }
 
 void AttributeFilter::addAttribute(std::string attribute) {
-    if (std::find(this->attributes.begin(), this->attributes.end(), attribute) != this->attributes.end()){
-        // if the attribute is not already in the vector
-        this->attributes.push_back(attribute);
+    if (!attribute.empty()){
+        if (std::find(this->attributes.begin(), this->attributes.end(), attribute) == this->attributes.end()){
+            // if the attribute is not already in the vector
+            this->attributes.push_back(attribute);
+        }
     }
 }
 
