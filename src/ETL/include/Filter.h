b0a2a906612b5546099a09c41e66ca1a3ee58b7f
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-03-03 19:18:42 +0100

    [feat] new signature and name for method apply/applyTo

--- src/ETL/include/Filter.h
+++ src/ETL/include/Filter.h
@@ -7,10 +7,12 @@
 
 
 #include <string>
+#include "../../Data/include/QueryBuilder.h"
+
 
 class Filter {
     public:
-        virtual std::string apply() = 0;
+        virtual void applyTo(QueryBuilder &qb) = 0;
 };
 
 
