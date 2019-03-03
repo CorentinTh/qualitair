3f27a91780db5f6a8cf255694910e19c1587b3bb
Sadikk <youpi.killeur@gmail.com>
2019-03-03 12:45:35 +0100

    [feat] ATMO computation

--- src/DataProcessor/src/Aggregation.cpp
+++ src/DataProcessor/src/Aggregation.cpp
@@ -116,17 +116,38 @@ std::unordered_map<std::string, double> Aggregation::computeDeviation() {
     return deviations;
 }
 
+static std::unordered_map<std::string, std::map<double,int>> atmoTable = {
+        { "O3",{
+                       {29, 1}, {54, 2}, {79, 3}, {104, 4}, {129, 5}, {149, 6}, {179, 7}, {209, 8}, {239, 9}
+               }
+        },
+        { "SO2",{
+                       {39, 1}, {79, 2}, {119, 3}, {159, 4}, {199, 5}, {249, 6}, {299, 7}, {399, 8}, {499, 9}
+               }
+        },
+        { "NO2",{
+                       {29, 1}, {54, 2}, {84, 3}, {109, 4}, {134, 5}, {164, 6}, {199, 7}, {274, 8}, {399, 9}
+               }
+        },
+        { "PM10",{
+                       {6, 1}, {13, 2}, {20, 3}, {27, 4}, {34, 5}, {41, 6}, {49, 7}, {64, 8}, {79, 9}
+               }
+        }
+};
+
 double Aggregation::computeAtmo() {
+    //contract : we always get data representing one day
 
     double pm10 = 0.0;
     double so2 = 0.0;
     double no2 = 0.0;
     double o3 = 0.0;
 
+
+    ///////// PM10 /////////
     /* moyenne de la journée */
-    //PM10
-    /*std::unordered_map<int, double> sums;
-    std::unordered_map<int, int> count;
+    double pm_sum = 0.0;
+    int pm_count = 0;
 
     for (auto i = points.begin(); i != points.end() ; ++i)
     {
@@ -137,49 +158,62 @@ double Aggregation::computeAtmo() {
                 for (std::unordered_map<std::string, int>::const_iterator it = k->begin();
                      it != k->end(); ++it)
                 {
-                    sums[it->first] += it->second;
-                    count[it->first]++;
-
+                    if (it->first == "PM10") {
+                        pm_sum += it->second;
+                        pm_count++;
+                    }
                 }
             }
         }
     }
+    pm10 = pm_sum / pm_count;
+    ///////// END PM10 /////////
 
-    //transform sums to mean
-    for (auto it = sums.begin(); it!= sums.end(); ++it) {
-        //round to 2 digits
-        sums[it->first] = std::floor((sums[it->first] / count[it->first]) * 100.0 + 0.5) / 100.0;
-    }*/
-
-
+    /////// SO3 - O3 - NO2 ////////
     /* moyenne des maxima par heure */
-    //SO3
-
-
-
-    //NO2
-
-    //O3
-
-    std::unordered_map<std::string, std::unordered_map<double,int>> atmoTable = {
-            { "O3",{
-                     {29, 1}, {54, 2}, {79, 3}, {104, 4}, {129, 5}, {149, 6}, {179, 7}, {209, 8}, {239, 9}
+    int iterPerHour = points.size() / 24;
+    std::unordered_map<int, std::unordered_map<std::string, double>> maxima;
+    std::unordered_map<std::string, double> means;
+    int currentIter = 0;
+    int currentHour = 0;
+    for (auto i = points.begin(); i != points.end() ; ++i) {
+        for (auto j = i->begin(); j != i->end() ; ++j) {
+            for (auto k = j->begin(); k != j->end() ; ++k) {
+                for (std::unordered_map<std::string, int>::const_iterator it = k->begin();
+                     it != k->end(); ++it) {
+                    //initial values
+                    if (!maxima[currentHour].count(it->first))
+                    {
+                        maxima[currentHour][it->first] = it->second;
+                    }
+                    //max
+                    if (it->second > maxima[currentHour][it->first]) {
+                        maxima[currentHour][it->first] = it->second;
+                    }
                 }
-            },
-            { "SO2",{
-                       {39, 1}, {79, 2}, {119, 3}, {159, 4}, {199, 5}, {249, 6}, {299, 7}, {399, 8}, {499, 9}
-                   }
-            },
-            { "NO2",{
-                       {29, 1}, {54, 2}, {84, 3}, {109, 4}, {134, 5}, {164, 6}, {199, 7}, {274, 8}, {399, 9}
-                   }
-            },
-            { "PM10",{
-                       {6, 1}, {13, 2}, {20, 3}, {27, 4}, {34, 5}, {41, 6}, {49, 7}, {64, 8}, {79, 9}
-                   }
             }
-    };
+        }
+        currentIter++;
+        if (currentIter == iterPerHour) {
+            currentIter = 0;
+            currentHour++;
+        }
+    }
+    //sum maximas by attribute
+    for (auto i = maxima.begin(); i != maxima.end(); ++i) {
+        for (auto j = i->second.begin(); j != i->second.end() ; ++j) {
+            means[j->first] += j->second;
+        }
+    }
+    //transform to means
+    for (auto i = means.begin(); i != means.end(); ++i) {
+        means[i->first] /= 24.0;
+    }
 
+    so2 = means["SO2"];
+    no2 = means["NO2"];
+    o3 = means["O3"];
+    /////// END SO3 - O3 - NO2 ////////
     int ind_pm10 = 0;
     int ind_so2 = 0;
     int ind_no2 = 0;
@@ -199,14 +233,14 @@ double Aggregation::computeAtmo() {
         }
     }
 
-    for(auto it = atmoTable["NO2"].begin(); it !=  atmoTable["NO2"].end() && ind_o3==0; ++it)
+    for(auto it = atmoTable["NO2"].begin(); it !=  atmoTable["NO2"].end() && ind_no2==0; ++it)
     {
         if (no2 < it->first) {
             ind_no2 = it->second;
         }
     }
 
-    for(auto it = atmoTable["PM10"].begin(); it !=  atmoTable["PM10"].end() && ind_o3==0; ++it)
+    for(auto it = atmoTable["PM10"].begin(); it !=  atmoTable["PM10"].end() && ind_pm10==0; ++it)
     {
         if (pm10 < it->first) {
             ind_pm10 = it->second;
