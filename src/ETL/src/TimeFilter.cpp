36c076ea5fb3f3003ac3de3735bb371e9535128f
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-03-04 20:06:41 +0100

    [fix] fixed implementation of TimeFilter

--- src/ETL/src/TimeFilter.cpp
+++ src/ETL/src/TimeFilter.cpp
@@ -3,31 +3,48 @@
 //
 
 #include "../include/TimeFilter.h"
+#include <stdexcept>
+
 
 void TimeFilter::applyTo(QueryBuilder &qb) {
     if (this->startDefined){
-        qb.andWhere("date > ?").bind(this->start);
+        qb.andWhere("timestamp > ?").bind(this->start);
     }
     if (this->endDefined){
-        qb.andWhere("date < ?").bind(this->end);
+        qb.andWhere("timestamp < ?").bind(this->end);
     }
 }
 
 void TimeFilter::setStart(std::time_t start) {
-    this->start = start;
-    this->startDefined = true;
+    if (!this->endDefined || (this->endDefined && start < this->end)){
+        this->start = start;
+        this->startDefined = true;
+    }
+    else{
+        throw std::logic_error("La date de debut est supérieure ou égale à la date de fin déjà renseignée");
+    }
 }
 
 void TimeFilter::setEnd(std::time_t end) {
-    this->end = end;
-    this->endDefined = true;
+    if (!this->startDefined || (this->startDefined && this->start < end)){
+        this->end = end;
+        this->endDefined = true;
+    }
+    else{
+        throw std::logic_error("La date de fin est inférieure ou égale à la date de début déjà renseignée");
+    }
 }
 
 void TimeFilter::setInterval(std::time_t start, std::time_t end) {
-    this->start = start;
-    this->end = end;
-    this->startDefined = true;
-    this->endDefined = true;
+    if (start < end){
+        this->start = start;
+        this->end = end;
+        this->startDefined = true;
+        this->endDefined = true;
+    }
+    else{
+        throw std::logic_error("L'intervalle passé en paramètre de setInterval n'est pas correct");
+    }
 }
 
 TimeFilter &TimeFilter::operator=(TimeFilter other) {
