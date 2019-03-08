f6d8267119f2654fa82aeed7a75265e2fb14da95
CorentinTh <corentin.thomasset74@gmail.com>
2019-03-08 19:32:45 +0100

    [refactor] switched to Measurement*

--- src/DataProcessor/include/IDataProcessor.h
+++ src/DataProcessor/include/IDataProcessor.h
@@ -23,7 +23,7 @@ class IDataProcessor {
         virtual json *computeDeviation(pointCollection &data) = 0;
         virtual json *computeAtmo(pointCollection &data) = 0;
         virtual json *computeExtrems(pointCollection &data) = 0;
-        virtual json *detectSimilar(std::vector<Measurement> measures, double threshold) = 0;
+        virtual json *detectSimilar(std::vector<Measurement*> measures, double threshold) = 0;
         virtual json *detectBroken(std::vector<Measurement> measures, int timeThreshold,
                            std::unordered_map<std::string, std::pair<int, int>> admissibleRanges) = 0;
         virtual json * detectSpikes(pointCollection *data, std::string attribute, double valueThreshold,
