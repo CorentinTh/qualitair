ec3995dd546ae415edc7cb09107302c9065a6c10
cmenseau <cyrielle.menseau@insa-lyon.fr>
2019-03-11 20:12:54 +0100

    [moche] pour que travis compile

--- tests/unit-tests/utils.test.cpp
+++ tests/unit-tests/utils.test.cpp
@@ -9,16 +9,14 @@ TEST_CASE("Test parseRFC3339Date to timestamp", "[UT-Utils-1]") {
     std::string date = "2019-03-10 10:36:15";
     long expectedTimestamp = 1552210575000;
 
-    CHECK(parseRFC3339Date(date) == expectedTimestamp);
-    CHECK(parseRFC3339Date("") == 0)
-    CHECK(parseRFC3339Date() == 0)
+    CHECK(utils::parseRFC3339Date(date) == expectedTimestamp);
+    CHECK(utils::parseRFC3339Date("") == 0);
 }
 
 TEST_CASE("Test parseISO8601Date to timestamp", "[UT-Utils-2]") {
     std::string date = "2017-01-01T16:31:14.2420000";
     long expectedTimestamp = 1483284674242;
 
-    CHECK(parseISO8601Date(date) == expectedTimestamp);
-    CHECK(parseISO8601Date("") == 0);
-    CHECK(parseISO8601Date() == 0);
+    CHECK(utils::parseISO8601Date(date) == expectedTimestamp);
+    CHECK(utils::parseISO8601Date("") == 0);
 }
