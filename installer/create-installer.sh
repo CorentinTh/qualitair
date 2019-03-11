cc27302570bd443965fc35ea04a05903e9e6c71f
CorentinTh <corentin.thomasset74@gmail.com>
2019-03-11 17:04:47 +0100

    [feat] installer now install html-templates

--- installer/create-installer.sh
+++ installer/create-installer.sh
@@ -3,6 +3,8 @@
 binaryName="qualitair"
 binaryPath="../bin/$binaryName"
 
+templatePath="../html-templates/dist"
+
 if [[ ! -f "$binaryPath" ]]
 then
 	echo "Error: $binaryPath not found. Has it been built ?"
@@ -10,13 +12,22 @@ then
 fi
 
 # Remove the binary from the archive
-rm -f archive/assets/${binaryName}
+rm -f "qualitair/assets/$binaryName"
+
+# Remove html-template from the archive
+rm -rf "qualitair/assets/html-templates"
+
+mkdir -p "qualitair/assets/html-templates"
 
 # Put the binary into the future archive folder
-cp -f ${binaryPath} archive/assets/${binaryName}
+cp -f ${binaryPath} "qualitair/assets/$binaryName"
+
+# Setting html-templates
+cp -f ${templatePath}/*.html "qualitair/assets/html-templates"
 
 # Zipping the content of the archive folder
-zip -r ../qualitair.zip qualitair
+zip -r "../qualitair.zip" "qualitair"
 
 # Remove the binary from the archive folder
-rm -f archive/assets/${binaryName}
+rm -f "archive/assets/$binaryName"
+
