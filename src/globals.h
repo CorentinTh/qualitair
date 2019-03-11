5ebca916b53d4db0dc8ca7c89ed94e4acf85578c
CorentinTh <corentin.thomasset74@gmail.com>
2019-03-11 23:06:35 +0100

    [refactor] globals.h: headers + auto indent

--- src/globals.h
+++ src/globals.h
@@ -1,3 +1,9 @@
+//
+//        ----[  QUALIT'AIR  ]----
+//
+//    Marsaud Menseau Thomasset Wallyn
+//  Copyright © 2019 - All right reserved
+//
 
 #ifndef QUALITAIR_GLOBALS_H
 #define QUALITAIR_GLOBALS_H
